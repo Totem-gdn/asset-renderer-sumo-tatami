21797b181b9f814ae1a36ca74ed4971645269213
illiadimension <ilya.nikulin@it-dimension.com>
2023-02-09 17:09:05 +0200

    fix info

--- controllers/api/info.js
+++ controllers/api/info.js
@@ -2,7 +2,7 @@
 
 class InfoController {
   get (req, res, next) {
-    res.status(200).json({"supported_asset_types": ["item"]})
+    res.status(200).json({"supported_asset_types": ["avatar"]})
   }
 }
 
